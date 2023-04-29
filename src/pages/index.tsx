77b5cd13cdf13f6f2c1aad3cdaff461cc6390414
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2023-04-29 17:28:33 +0200

    test protected branch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <h1>Casba Media</h1>
+        <h1>Casba Media!!!</h1>
       </main>
     </>
   )
